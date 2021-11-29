74eeb68dfdfc099db0cd396cf5ba57d6f4e71dfb
Simon <38482259+21wang12@users.noreply.github.com>
2021-11-29 14:24:47 +0800

    Update email.js

--- email.js
+++ email.js
@@ -155,5 +155,6 @@ email_new.push("1192767307@qq.com")
 email_new.push("1098125831@qq.com")
 email_new.push("875114965@qq.com")
 email_new.push("celine1120@126.com")
+email_new.push("122582186@qq.com")
 
 
